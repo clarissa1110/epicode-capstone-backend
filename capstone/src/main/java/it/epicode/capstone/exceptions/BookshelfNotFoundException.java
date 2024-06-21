c28f4d3db8619d259fb47449bb93de48bde33eee
clarissa1110 <157387643+clarissa1110@users.noreply.github.com>
2024-06-21 19:17:24 +0200

    Revert "Update  update book management"

--- capstone/src/main/java/it/epicode/capstone/exceptions/BookshelfNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package it.epicode.capstone.exceptions;
-
-public class BookshelfNotFoundException extends RuntimeException{
-    public BookshelfNotFoundException(String message) {
-        super(message);
-    }
-}
--- capstone/src/main/java/it/epicode/capstone/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package it.epicode.capstone.repositories;
-
-import it.epicode.capstone.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
